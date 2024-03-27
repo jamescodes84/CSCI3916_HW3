b03b3807cc9addd229ad0a82b74fcfbbdfef06fc
jamescodes84 <james.reynolds9@icloud.com>
2024-03-27 16:49:40 -0600

    final

--- Users.js
+++ Users.js
@@ -5,7 +5,14 @@ var bcrypt = require('bcrypt-nodejs');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.DB).catch(error => (console.log(error)));
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
   
 
 //mongose.set('useCreateIndex', true);
